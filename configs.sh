b8c389d51402b18ee19eefb779bf54c127d10eab
phillyqueso <pjacobs@xeekr.com>
2013-05-15 18:07:02 -0500

    doing a . instead of source

--- configs.sh
+++ configs.sh
@@ -1,5 +1,5 @@
 
-export LIBPNG_VERSION="1.5.14"
+export LIBPNG_VERSION="1.6.2"
 export IMAGE_MAGICK_VERSION="6.8.4-10"
 export REMOTE_PATH="imagemagick-${IMAGE_MAGICK_VERSION}.tgz"
 export LIBPNG_REMOTE_PATH="libpng-${LIBPNG_VERSION}.tgz"
