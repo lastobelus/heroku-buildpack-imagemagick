b155d8cd332bb6aa0b375965bd71dee088fa8e10
Michael Johnston <lastobelus@mac.com>
2013-10-11 04:48:41 -0700

    use webify repo. basedir fix. latest IM, libpng

--- configs.sh
+++ configs.sh
@@ -1,6 +1,6 @@
 
-export LIBPNG_VERSION="1.6.2"
-export IMAGE_MAGICK_VERSION="6.8.4-10"
+export LIBPNG_VERSION="1.6.6"
+export IMAGE_MAGICK_VERSION="6.8.7-0"
 export REMOTE_PATH="imagemagick-${IMAGE_MAGICK_VERSION}.tgz"
 export LIBPNG_REMOTE_PATH="libpng-${LIBPNG_VERSION}.tgz"
-export S3_BUCKET="com.tastemade.buildpack-imagemagick"
+export S3_BUCKET="com.webify.buildpack-imagemagick"
