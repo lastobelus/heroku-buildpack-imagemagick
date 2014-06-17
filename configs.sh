565721bb405d2fcf1dfc6f5c4c7eea6ba652a995
f-kubotar <f.kubotar@paperboy.co.jp>
2014-06-17 17:25:01 +0900

    add jpeg-archive

--- configs.sh
+++ configs.sh
@@ -2,10 +2,12 @@
 export LIBPNG_VERSION="1.6.12"
 export NASM_VERSION="2.11.05"
 export LIBJPEG_TURBO_VERSION="1.3.1"
+export JPEG_ARCHIVE_VERSION="09a130be1c"
 export IMAGE_MAGICK_VERSION="6.8.9-3"
 export REMOTE_PATH="imagemagick-${IMAGE_MAGICK_VERSION}.tgz"
 export LIBPNG_REMOTE_PATH="libpng-${LIBPNG_VERSION}.tgz"
 export NASM_REMOTE_PATH="nasm-${NASM_VERSION}.tgz"
 export LIBJPEG_TURBO_REMOTE_PATH="libjpeg-turbo-${LIBJPEG_TURBO_VERSION}.tgz"
+export JPEG_ARCHIVE_REMOTE_PATH="jpeg-archive-${JPEG_ARCHIVE_VERSION}.tgz"
 export S3_BUCKET="suzuri-buildpack-imagemagick"
 
